fafeed0e9d6b7b0c09b5274fea83447fb9f8ad19
Leniors <mokualeeroy@gmail.com>
2023-07-17 07:35:27 -0700

    exists.c

---
+++ exists.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copy str
+ *@dest: copy str from here
+ *@src: to here
+ *@n: num of char to copy
+ *Return: concatenated str
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - joins 2 str
+ *@dest: 1st str
+ *@src: 2nd str
+ *@n: no of bytes to use
+ *Return: joined str
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - find char in str
+ *@s: str in question
+ *@c: char to find
+ *Return: (s) pointer as memory of s
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
+
